2658822652ead383aa67867e59ae9e08f6fdc799
Alex Rock <hi@lxrck.com>
2015-04-08 17:01:14 -0500

    Assets Changes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var path = require('path');
-var port     = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 require('./routes/index')(app);
 
